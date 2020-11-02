1f2ef0b5fd8f09b534751058c35c2e48ed6ae950
eduviictor <eduvictornobrega@gmail.com>
2020-11-02 13:59:15 -0300

    feat: add middleware for error handling

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import { router } from './routes';
 
@@ -6,11 +6,19 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(router);
 
-app.use((req: Request, res: Response, next) => {
+app.use((req: Request, res: Response, next: NextFunction) => {
   res.set('X-Powered-By', 'PHP/7.1.7');
   next();
+});
+
+app.use(router);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error(err.stack);
+  res.status(500).json({
+    message: 'There was a problem performing the operation. Try later.'
+  });
 });
 
 export { app };
